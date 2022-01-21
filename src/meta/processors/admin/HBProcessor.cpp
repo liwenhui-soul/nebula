aaa7731b480156b7ea56abbc94ff506ab8c526d3
nebula-bots <88429921+nebula-bots@users.noreply.github.com>
2022-01-21 12:15:37 +0800

    add comments and adjust log level for meta/processors/admin (#514)
    <!--
    Thanks for your contribution!
    In order to review PR more efficiently, please add information according to the template.
    -->
    
    #### What type of PR is this?
    - [ ] bug
    - [ ] feature
    - [x] enhancement
    
    #### What problem(s) does this PR solve?
    Issue(s) number: 
    
    Description:
    Adjust codes in src/meta/processors/admin.:
    1. change all ERROR to INFO
    2. add comments for public function and some classes.
    
    #### How do you solve it?
    
    
      
    #### Special notes for your reviewer, ex. impact of this fix, design document, etc:
    
    
    
    #### Checklist:
    Tests:
    - [ ] Unit test(positive and negative cases)
    - [ ] Function test
    - [ ] Performance test
    - [ ] N/A
    
    Affects:
    - [ ] Documentation affected (Please add the label if documentation needs to be modified.)
    - [ ] Incompatibility (If it breaks the compatibility, please describe it and add the label.）
    - [ ] If it's needed to cherry-pick (If cherry-pick to some branches is required, please label the destination version(s).)
    - [ ] Performance impacted: Consumes more CPU/Memory
    
    
    #### Release notes:
    
    Please confirm whether to be reflected in release notes and how to describe:
    >  No need
    
    
    Migrated from vesoft-inc/nebula#3662
    
    Co-authored-by: pengwei.song <90180021+pengweisong@users.noreply.github.com>

--- src/meta/processors/admin/HBProcessor.cpp
+++ src/meta/processors/admin/HBProcessor.cpp
@@ -37,7 +37,7 @@ void HBProcessor::process(const cpp2::HBReq& req) {
       role == cpp2::HostRole::STORAGE_LISTENER) {
     if (role == cpp2::HostRole::STORAGE) {
       if (!ActiveHostsMan::machineRegisted(kvstore_, host)) {
-        LOG(ERROR) << "Machine " << host << " is not registed";
+        LOG(INFO) << "Machine " << host << " is not registed";
         handleErrorCode(nebula::cpp2::ErrorCode::E_MACHINE_NOT_FOUND);
         onFinished();
         return;
@@ -50,7 +50,7 @@ void HBProcessor::process(const cpp2::HBReq& req) {
       LOG(INFO) << "Set clusterId for new host " << host << "!";
       resp_.cluster_id_ref() = clusterId_;
     } else if (peerClusterId != clusterId_) {
-      LOG(ERROR) << "Reject wrong cluster host " << host << "!";
+      LOG(INFO) << "Reject wrong cluster host " << host << "!";
       handleErrorCode(nebula::cpp2::ErrorCode::E_WRONGCLUSTER);
       onFinished();
       return;
