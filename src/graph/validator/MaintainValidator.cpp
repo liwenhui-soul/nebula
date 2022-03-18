d3cf5356104c833466135872db2faf2129cc0959
yaphet <4414314+darionyaphet@users.noreply.github.com>
2022-03-18 21:01:10 +0800

    Revert "disable add hosts into zone (#560)" (#688)
    This reverts commit fa968ecf7fd7a06f7c906e129d38386dd6ebaf5c.
    
    Co-authored-by: Sophie <84560950+Sophie-Xie@users.noreply.github.com>

--- src/graph/validator/MaintainValidator.cpp
+++ src/graph/validator/MaintainValidator.cpp
@@ -575,7 +575,7 @@ Status ShowZonesValidator::toPlan() {
 }
 
 Status AddHostsIntoZoneValidator::validateImpl() {
-  return Status::SemanticError("Add hosts into zone is unsupported");
+  return Status::OK();
 }
 
 Status AddHostsIntoZoneValidator::toPlan() {
