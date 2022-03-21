b00321b1ce2eaa0edbb3c46cb9aa20446ba5a387
nebula-bots <88429921+nebula-bots@users.noreply.github.com>
2022-03-21 12:26:22 +0800

    cleanWal by commitLogId (#713)
    Co-authored-by: liwenhui-soul <38217397+liwenhui-soul@users.noreply.github.com>

--- src/kvstore/NebulaStore.cpp
+++ src/kvstore/NebulaStore.cpp
@@ -1285,7 +1285,7 @@ void NebulaStore::cleanWAL() {
       auto& part = partEntry.second;
       if (part->needToCleanWal()) {
         // clean wal by expired time
-        part->wal()->cleanWAL();
+        part->cleanWal();
       }
     }
   }
@@ -1293,8 +1293,8 @@ void NebulaStore::cleanWAL() {
     for (const auto& partEntry : spaceEntry.second->listeners_) {
       for (const auto& typeEntry : partEntry.second) {
         const auto& listener = typeEntry.second;
-        // clean wal by log id
-        listener->wal()->cleanWAL(listener->getApplyId());
+        // clean wal by commit log id
+        listener->cleanWal();
       }
     }
   }
