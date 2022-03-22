3a4828e08025186db29bfe61feb431eb80971f1b
nebula-bots <88429921+nebula-bots@users.noreply.github.com>
2022-03-22 13:59:46 +0800

    fix kv snapshot data inconsist (#722)
    Co-authored-by: kikimo <451943+kikimo@users.noreply.github.com>
    Co-authored-by: Doodle <13706157+critical27@users.noreply.github.com>
    
    Co-authored-by: liwenhui-soul <38217397+liwenhui-soul@users.noreply.github.com>
    Co-authored-by: kikimo <451943+kikimo@users.noreply.github.com>
    Co-authored-by: Doodle <13706157+critical27@users.noreply.github.com>

--- src/common/utils/NebulaKeyUtils.h
+++ src/common/utils/NebulaKeyUtils.h
@@ -90,6 +90,7 @@ class NebulaKeyUtils final {
   static std::string systemPartKey(PartitionID partId);
 
   static std::string kvKey(PartitionID partId, const folly::StringPiece& name);
+  static std::string kvPrefix(PartitionID partId);
 
   /**
    * Prefix for tag
